349391ac8573215028a46434705b017677c607ea
Kelly Wang <kwang6@stuy.edu>
2016-09-30 12:44:18 -0400

    moved everything from old dir, IT WORKSdeactivate!

---
+++ appHW.py
@@ -0,0 +1,31 @@
+import random
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("button.html")
+
+@app.route("/auth", methods=['POST'])
+def authenticate():
+    print "\n\n\n"
+    print ":::DIAG::: this Flask obj"
+    print app
+    print "request"
+    print request
+    print "headers"
+    print request.headers
+    print "methods"
+    print request.method
+    print "argss"
+    ##print request.args['KELLY']
+    status = "failure"
+    if request.form['username'] == "JERRY":
+        status = "success"
+        
+    return render_template("sOrF.html", sOrF=status)
+    
+
+if __name__=="__main__":
+    app.debug = True
+    app.run()
